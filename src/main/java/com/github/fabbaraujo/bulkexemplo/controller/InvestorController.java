a7ca1fdd7d5f51376328a1f57eb08a12a1e41fee
Fabrício Araújo <fabaraujo23@gmail.com>
2021-01-06 17:24:45 -0300

    exemplo de bulk

---
+++ src/main/java/com/github/fabbaraujo/bulkexemplo/controller/InvestorController.java
@@ -0,0 +1,80 @@
+package com.github.fabbaraujo.bulkexemplo.controller;
+
+import com.github.fabbaraujo.bulkexemplo.model.Stock;
+import com.github.fabbaraujo.bulkexemplo.model.exceptions.CustomHeaderNotFoundException;
+import com.github.fabbaraujo.bulkexemplo.service.InvestorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@ComponentScan(basePackageClasses = { InvestorService.class })
+public class InvestorController {
+
+    private static final String ID = "/{id}";
+    @Autowired
+    private InvestorService investorService;
+
+    @PostMapping("/investors/{investorId}/stocks")
+    public ResponseEntity<Void> addNewStockToTheInvestorPortfolio(@PathVariable String investorId,
+                                                                  @RequestBody Stock newStock) {
+        Stock insertedStock = investorService.addNewStockToTheInvestorPortfolio(investorId, newStock);
+        if (insertedStock == null) {
+            return ResponseEntity.noContent().build();
+        }
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path(ID)
+                .buildAndExpand(insertedStock.getSymbol()).toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @PutMapping("/investors/{investorId}/stocks")
+    public ResponseEntity<Void> updateAStockOfTheInvestorPortfolio(@PathVariable String investorId,
+                                                                   @RequestBody Stock stockTobeUpdated) {
+        Stock updatedStock = investorService.updateAStockByInvestorIdAndStock(investorId, stockTobeUpdated);
+        if (updatedStock == null) {
+            return ResponseEntity.noContent().build();
+        }
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path(ID)
+                .buildAndExpand(updatedStock.getSymbol()).toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @PatchMapping("/investors/{investorId}/stocks")
+    public ResponseEntity<Void> updateStockOfTheInvestorPortfolio(@PathVariable String investorId,
+                                                                  @RequestHeader(value = "x-bulk-patch") Optional<Boolean> isBulkPatch,
+                                                                  @RequestBody List<Stock> stocksTobeUpdated) throws CustomHeaderNotFoundException {
+        if (!isBulkPatch.isPresent()) {
+            throw new CustomHeaderNotFoundException("x-bulk-patch not found in your headers");
+        }
+        investorService.bulkUpdateOfStocksByInvestorId(investorId, stocksTobeUpdated);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/investors/{investorId}/stocks/{symbol}")
+    public ResponseEntity<Void> updateAStockOfTheInvestorPortfolio(@PathVariable String investorId,
+                                                                   @PathVariable String symbol, @RequestBody Stock stockTobeUpdated) {
+        Stock updatedStock = investorService.updateAStockByInvestorIdAndStock(investorId, symbol, stockTobeUpdated);
+        if (updatedStock == null) {
+            return ResponseEntity.noContent().build();
+        }
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path(ID)
+                .buildAndExpand(updatedStock.getSymbol()).toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/investors/{investorId}/stocks/{symbol}")
+    public ResponseEntity<Void> deleteAStockFromTheInvestorPortfolio(@PathVariable String investorId,
+                                                                     @PathVariable String symbol) {
+        if (investorService.deleteStockFromTheInvestorPortfolio(investorId, symbol)) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(null);
+    }
+}
---
+++ src/main/java/com/github/fabbaraujo/bulkexemplo/model/exceptions/CustomHeaderNotFoundException.java
@@ -0,0 +1,9 @@
+package com.github.fabbaraujo.bulkexemplo.model.exceptions;
+
+public class CustomHeaderNotFoundException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public CustomHeaderNotFoundException(String exception) {
+        super(exception);
+    }
+}
