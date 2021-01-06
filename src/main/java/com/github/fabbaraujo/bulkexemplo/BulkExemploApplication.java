91235b62269f93258594fe62564cc1e8231eaeca
Fabrício Araújo <fabaraujo23@gmail.com>
2021-01-06 17:16:04 -0300

    First commit

---
+++ src/main/java/com/github/fabbaraujo/bulkexemplo/BulkExemploApplication.java
@@ -0,0 +1,13 @@
+package com.github.fabbaraujo.bulkexemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulkExemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulkExemploApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/fabbaraujo/bulkexemplo/BulkExemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fabbaraujo.bulkexemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulkExemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
